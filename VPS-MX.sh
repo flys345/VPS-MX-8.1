2aca354fda67c4bae252c3c3f5d77684237b5bfc
/ VPS-MX <31332664+VPS-MX@users.noreply.github.com>
2020-06-18 16:12:03 -0500

    Update VPS-MX.sh
    1.4

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -350,7 +350,7 @@ wget -O $HOME/lista-arq $(ofus "$Key")/$IP > /dev/null 2>&1 && echo -e "\033[1;3
    }
 IP=$(ofus "$Key" | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}') && echo "$IP" > /usr/bin/vendor_code
 sleep 1s
-function_verify
+#function_verify
 updatedb
 if [[ -e $HOME/lista-arq ]] && [[ ! $(cat $HOME/lista-arq|grep "KEY INVALIDA!") ]]; then
    msg -bar2
