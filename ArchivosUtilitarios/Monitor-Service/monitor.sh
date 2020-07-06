8ee08f8d24c415998efa16a820324e5b1f917b1a
/ VPS-MX <31332664+VPS-MX@users.noreply.github.com>
2020-07-06 16:15:42 -0500

    @Kalix1
    v8.0

--- ArchivosUtilitarios/Monitor-Service/monitor.sh
+++ ArchivosUtilitarios/Monitor-Service/monitor.sh
@@ -39,7 +39,7 @@ echo "
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-  <title>Monitor de Servicios By @Kalix1</title>
+  <title>Monitor de Servicios VPS-MX</title>
   <link rel='stylesheet' href='estilos.css'>
 </head>
 <body>
