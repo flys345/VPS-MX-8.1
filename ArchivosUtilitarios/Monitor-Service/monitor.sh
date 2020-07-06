b3af80e24248574ebf9fbf782a352ec8b43f1a05
/ VPS-MX <31332664+VPS-MX@users.noreply.github.com>
2020-07-06 15:06:10 -0500

    @Kalix1
    v8.0

--- ArchivosUtilitarios/Monitor-Service/monitor.sh
+++ ArchivosUtilitarios/Monitor-Service/monitor.sh
@@ -20,12 +20,12 @@ EstadoServicio() {
 		service $1 restart && 
 		NOM="$(less /etc/newadm/ger-user/nombre.log)"
         NOM1="$(echo $NOM)"
-		USERID="475422642"
+		IDB="$(less /etc/newadm/ger-user/IDT.log)" 
+        USERID="$(echo $IDB)"
         KEY="862633455:AAGJ9BBJanzV6yYwLSemNAZAVwn7EyjrtcY"
         URL="https://api.telegram.org/bot$KEY/sendMessage"
         MSG="⚠️ AVISO DE VPS: $NOM1 ⚠️
 		❗️Service $1 con fallo / Reiniciando❗️"
-		
 		curl -s --max-time 10 -d "chat_id=$USERID&disable_web_page_preview=1&text=$MSG" $URL
     fi
 }
@@ -54,7 +54,7 @@ EstadoServicio ssh
 # Servidor DROPBEAR
 EstadoServicio dropbear
 # El servidor SSL
-EstadoServicio stunnel4 
+EstadoServicio stunnel4
 # Servidor SQUID
 [[ $(EstadoServicio squid) ]] && EstadoServicio squid3
 # Servidor APACHE
@@ -63,8 +63,8 @@ on="<span class='encendido'> ACTIVO " && off="<span class='detenido'> DESACTIVAD
 [[ $(ps x | grep badvpn | grep -v grep | awk '{print $1}') ]] && badvpn=$on || badvpn=$off
 echo "<p>Estado del servicio badvpn está ||  $badvpn </span>.</p> " >> $DIR/$ARCHIVO
 #SERVICE BADVPN
-PIDVRF3="$(ps aux|grep badvpn |grep -v grep|awk '{print $2}')"
 
+PIDVRF3="$(ps aux|grep badvpn |grep -v grep|awk '{print $2}')"
 if [[ -z $PIDVRF3 ]]; then
 screen -dmS badvpn2 /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
 NOM="$(less /etc/newadm/ger-user/nombre.log)"
@@ -80,8 +80,8 @@ for pid in $(echo $PIDVRF3); do
 echo""
 done
 fi
-#SERVICE PYTHON DIREC
 
+#SERVICE PYTHON DIREC
 ureset_python () {
 for port in $(cat /etc/newadm/PortPD.log| grep -v "nobody" |cut -d' ' -f1)
 do
